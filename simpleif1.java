7ea02417f2cc45f8ccad482a77cd4f5ebfb5fefe
Abizeyimanapascal <abizeyimanapascal0@gmail.com>
2023-11-07 12:22:46 +0200

    Create simpleif1.java
    add another if program

---
+++ simpleif1.java
@@ -0,0 +1,11 @@
+// program to ckheck whether a number is even or odd
+public class simpleif1 { // main class
+    public static void main(String[] args) { // mein method
+        int number =6; // variable decralation
+        if (number%2==0) { // if condition
+            System.out.println("EVEN"); // output if condition is true
+        } else {
+            System.out.println("ODD"); // output if condion is false
+        }
+    }
+}
